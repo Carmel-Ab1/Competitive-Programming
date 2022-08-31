b41b4802dfcbeaddb42d75a15a727c66053466ac
Carmel-Ab1 <107980783+Carmel-Ab1@users.noreply.github.com>
2022-08-31 12:26:43 +0300

    Create D.cpp

---
+++ CodeForces/Round-1475/D.cpp
@@ -0,0 +1,121 @@
+/*
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+ */
+#include<bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+
+//using namespace __gnu_pbds;
+using namespace std;
+
+typedef long double ld;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int>vi;
+typedef vector<vector<int>>vvi;
+typedef vector<ll>vl;
+typedef vector<vl> vvl;
+typedef pair<int,int>pi;
+typedef pair<ll,ll> pl;
+typedef vector<pl> vpl;
+typedef vector<ld> vld;
+typedef pair<ld,ld> pld;
+typedef vector<pi> vpi;
+
+//typedef tree<ll, null_type, less_equal<ll>,rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+template<typename T> ostream& operator<<(ostream& os, vector<T>& a){os<<"[";for(int i=0; i<ll(a.size()); i++){os << a[i] << ((i!=ll(a.size()-1)?" ":""));}os << "]\n"; return os;}
+
+#define all(x) x.begin(),x.end()
+#define YES out("YES")
+#define NO out("NO")
+#define out(x){cout << x << "\n"; return;}
+#define outfl(x){cout << x << endl;return;}
+#define GLHF ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define print(x){for(auto ait:x) cout << ait << " "; cout << "\n";}
+#define pb push_back
+#define umap unordered_map
+
+template<typename T1, typename T2> istream& operator>>(istream& is, pair<T1, T2>& p){is >> p.first >> p.second;return is;}
+template<typename T1, typename T2> ostream& operator<<(ostream& os, pair<T1, T2>& p){os <<"" << p.first << " " << p.second << ""; return os;}
+void usaco(string taskname){
+    string fin = taskname + ".in";
+    string fout = taskname + ".out";
+    const char* FIN = fin.c_str();
+    const char* FOUT = fout.c_str();
+    freopen(FIN, "r", stdin);
+    freopen(FOUT, "w", stdout);
+}
+template<typename T>
+void read(vector<T>& v){
+    int n=v.size();
+    for(int i=0; i<n; i++)
+        cin >> v[i];
+}
+template<typename T>
+vector<T>UNQ(vector<T>a){
+    vector<T>ans;
+    for(T t:a)
+        if(ans.empty() || t!=ans.back())
+            ans.push_back(t);
+    return ans;
+}
+
+
+
+void solve();
+int main(){
+    GLHF;
+    int t=1;
+    cin >> t;
+    while(t--)
+        solve();
+}
+void solve() {
+    ll n,m;
+    cin >> n >> m;
+    vpl a(n);
+    for(int i=0; i<n; i++)
+        cin >> a[i].first;
+    for(int i=0; i<n; i++)
+        cin >> a[i].second;
+
+
+    vl ones,twos;
+    ll sum=0,ans=1e18;
+    for(pl p:a){
+        if(p.second==1)
+            ones.push_back(p.first);
+        else
+            twos.push_back(p.first);
+        sum+=p.first;
+    }
+    if(sum<m)out(-1)
+    sort(all(ones),greater<ll>());
+    sort(all(twos),greater<ll>());
+
+    vl pref1(ones.size()),pref2(twos.size());
+
+    for(int i=0; i<ones.size() ;i++)
+        pref1[i]=(i?pref1[i-1]:0)+ones[i];
+    for(int i=0; i<twos.size(); i++)
+        pref2[i]=(i?pref2[i-1]:0)+twos[i];
+
+    for(int i=0; i<=ones.size(); i++){
+        ll has=i?pref1[i-1]:0;
+        ll j=-1,l=0,r=twos.size();
+        while(l<=r){
+            ll mid=r-(r-l)/2;
+            if(m<=has+(mid?pref2[mid-1]:0)){
+                r=mid-1;
+                j=mid;
+            }
+            else
+                l=mid+1;
+        }
+        if(j!=-1)
+            ans=min(ans,i+2*j);
+    }
+    out(ans)
+}
