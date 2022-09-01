9365d7f15148c2c5d65717412b1b481bc8384ae4
Carmel-Ab1 <107980783+Carmel-Ab1@users.noreply.github.com>
2022-09-01 17:44:01 +0300

    Create SIMILARSUBSQ.cpp

---
+++ CodeChef/JULY-LONG-22-2/SIMILARSUBSQ.cpp
@@ -0,0 +1,146 @@
+/*
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+ */
+#include<bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+
+//using namespace __gnu_pbds;
+using namespace std;
+
+typedef long double ld;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int>vi;
+typedef vector<vector<int>>vvi;
+typedef vector<ll>vl;
+typedef vector<vl> vvl;
+typedef pair<int,int>pi;
+typedef pair<ll,ll> pl;
+typedef vector<pl> vpl;
+typedef vector<ld> vld;
+typedef pair<ld,ld> pld;
+typedef vector<pi> vpi;
+
+//typedef tree<ll, null_type, less_equal<ll>,rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+template<typename T> ostream& operator<<(ostream& os, vector<T>& a){os<<"[";for(int i=0; i<ll(a.size()); i++){os << a[i] << ((i!=ll(a.size()-1)?" ":""));}os << "]\n"; return os;}
+
+#define all(x) x.begin(),x.end()
+#define YES out("YES")
+#define NO out("NO")
+#define out(x){cout << x << "\n"; return;}
+#define outfl(x){cout << x << endl;return;}
+#define GLHF ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define print(x){for(auto ait:x) cout << ait << " "; cout << "\n";}
+#define pb push_back
+#define umap unordered_map
+
+template<typename T1, typename T2> istream& operator>>(istream& is, pair<T1, T2>& p){is >> p.first >> p.second;return is;}
+template<typename T1, typename T2> ostream& operator<<(ostream& os, pair<T1, T2>& p){os <<"" << p.first << " " << p.second << ""; return os;}
+void usaco(string taskname){
+    string fin = taskname + ".in";
+    string fout = taskname + ".out";
+    const char* FIN = fin.c_str();
+    const char* FOUT = fout.c_str();
+    freopen(FIN, "r", stdin);
+    freopen(FOUT, "w", stdout);
+}
+template<typename T>
+void read(vector<T>& v){
+    int n=v.size();
+    for(int i=0; i<n; i++)
+        cin >> v[i];
+}
+template<typename T>
+vector<T>UNQ(vector<T>a){
+    vector<T>ans;
+    for(T t:a)
+        if(ans.empty() || t!=ans.back())
+            ans.push_back(t);
+    return ans;
+}
+
+
+
+void solve();
+int main(){
+    GLHF;
+    int t=1;
+    cin >> t;
+    while(t--)
+        solve();
+}
+using cd = complex<double>;
+const double PI = acos(-1);
+
+void fft(vector<cd> & a, bool invert) {
+    int n = a.size();
+    if (n == 1)
+        return;
+
+    vector<cd> a0(n / 2), a1(n / 2);
+    for (int i = 0; 2 * i < n; i++) {
+        a0[i] = a[2*i];
+        a1[i] = a[2*i+1];
+    }
+    fft(a0, invert);
+    fft(a1, invert);
+
+    double ang = 2 * PI / n * (invert ? -1 : 1);
+    cd w(1), wn(cos(ang), sin(ang));
+    for (int i = 0; 2 * i < n; i++) {
+        a[i] = a0[i] + w * a1[i];
+        a[i + n/2] = a0[i] - w * a1[i];
+        if (invert) {
+            a[i] /= 2;
+            a[i + n/2] /= 2;
+        }
+        w *= wn;
+    }
+}
+vector<int> multiply(vector<int> const& a, vector<int> const& b) {
+    vector<cd> fa(a.begin(), a.end()), fb(b.begin(), b.end());
+    int n = 1;
+    while (n < a.size() + b.size())
+        n <<= 1;
+    fa.resize(n);
+    fb.resize(n);
+
+    fft(fa, false);
+    fft(fb, false);
+    for (int i = 0; i < n; i++)
+        fa[i] *= fb[i];
+    fft(fa, true);
+
+    vector<int> result(n);
+    for (int i = 0; i < n; i++)
+        result[i] = round(fa[i].real());
+    return result;
+}
+void solve() {
+    int n;
+    cin >> n;
+    vi a(n);
+    read(a);
+
+    vi first(n+1,1e9),last(n+1);
+    for(int i=0; i<n; i++){
+        last[a[i]]=i;
+        first[a[i]]=min(first[a[i]],i);
+    }
+
+
+   vi P(2*n),Q(2*n);
+    for(int i=0; i<n;i++){
+        if(i==first[a[i]])
+            P[n-i]++;
+        if(i==last[a[i]])
+            Q[i]++;
+    }
+    vi R= multiply(P,Q);
+    for(int i=n,j=n; i<2*n ;j--,i++)
+        cout << (j-R[i]) << " ";
+    out("")
+}
